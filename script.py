ed66733e6c3e9b5cc724f0ace8ec84a15e2629b8
Kishore Kumar K-Manager-IT-Chennai-Chola <kishorek@cholamandalam.com>
2018-01-03 18:35:35 +0530

    test

--- script.py
+++ script.py
@@ -10,8 +10,8 @@ def plot():
     from bokeh.embed import components
     from bokeh.resources import CDN
 
-    start=datetime.datetime(2015,10,1)
-    end=datetime.datetime(2016,3,10)
+    start=datetime.datetime(2016,10,1)
+    end=datetime.datetime(2017,3,10)
 
     df=data.DataReader(name="GOOG",data_source="yahoo",start=start,end=end)
 
